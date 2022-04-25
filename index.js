4c84a5e65e0494b87eb2777eae450e9ef1d3bb54
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-04-25 12:26:05 +0200

    movieslist

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const express = require('express') ;
-const app = express() ;
-const port = 3000 ;
-
+const express = require("express");
 const movies = require("./movies");
+const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favourite movie list");
 });
 
-app.get("/movies",(req, res) => {
-    res.status(200).json(movies);
+app.get("/api/movies", (req, res) => {
+  res.status(200).json(movies);
 });
 
-app.get("/movies/:id",(req, res) => {
-    res.send(`${req.params.id}`);
-    movies.find(id, function (id) {
-      if (id) {
-        res.status(200).json(movies.id);
-      } else {
-        res.status(404).send("Not found");
-      }
-    })
+app.get("/api/movies/:id", (req, res) => {
+  const id = req.params.id;
+  if (movies[id]) {
+    res.status(200).json(movies[id]);
+  } else {
+    res.status(404).send("not found");
+  }
 });
 
 app.listen(port, (err) => {
-    if (err) {
-        console.error('Something bad happened');
-    } else {
-        console.log(`Server is listening on ${port}`);
-    }
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
 });
-
